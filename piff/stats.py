fixing some minor bugs, removing logger in gradient, adding max calls to fitters with marginal success

piff/stats.py
@@ -155,7 +155,7 @@ class Stats(object):
         logger.debug("Measuring shapes of real stars")
         shapes_truth = np.array([ piff.util.hsm(star) for star in stars ])
         for star, shape in zip(stars, shapes_truth):
-            logger.debug("real shape for star at %s is %s",star.image_pos, shape)
+            logger.log(5, "real shape for star at %s is %s",star.image_pos, shape)
 
         # Pull out the positions to return
         positions = np.array([ (star.data.properties['u'], star.data.properties['v'])
@@ -165,7 +165,7 @@ class Stats(object):
         logger.debug("Generating and Measuring Model Stars")
         shapes_model = np.array([ piff.util.hsm(psf.drawStar(star)) for star in stars ])
         for star, shape in zip(stars, shapes_model):
-            logger.debug("model shape for star at %s is %s",star.image_pos, shape)
+            logger.log(5, "model shape for star at %s is %s",star.image_pos, shape)
 
         return positions, shapes_truth, shapes_model
 